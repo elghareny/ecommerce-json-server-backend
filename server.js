3055248dc4a2f894d84f16382e8673e96bfe3074
Mohamed Said <74096301+elghareny@users.noreply.github.com>
2025-01-15 19:30:34 +0200

    Update server

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const jsonServer = require('json-server');
+const auth = require('json-server-auth')
 const server = jsonServer.create();
 const router = jsonServer.router('db.json');
+server.db = router.db
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 3200;
 
 server.use(middlewares);
+server.use(auth);
 server.use(router);
 server.listen(port);
